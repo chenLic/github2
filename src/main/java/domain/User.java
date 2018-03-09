b634350a4cd7d4974716f09c805b2275e12a37d8
chenLic <1105582945@qq.com>
2018-03-09 17:03:18 +0800

    添加asd dddd

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -4,6 +4,15 @@ public class User {
     private int id;
     private String name;
    private int asd;
+   private int das;
+
+    public int getDas() {
+        return das;
+    }
+
+    public void setDas(int das) {
+        this.das = das;
+    }
 
     public int getAsd() {
         return asd;
