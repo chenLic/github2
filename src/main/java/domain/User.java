fb7c0d7e0d0c703bb142c89942f97520b3174274
chenLic <1105582945@qq.com>
2018-03-09 17:00:48 +0800

    添加asd

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -2,9 +2,28 @@ package domain;
 
 public class User {
     private int id;
+    private String name;
+   private int asd;
+
+    public int getAsd() {
+        return asd;
+    }
+
+    public void setAsd(int asd) {
+        this.asd = asd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
+
     }
 
     public void setId(int id) {
